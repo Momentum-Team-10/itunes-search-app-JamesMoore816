9995aa16745b5b82bf6f01cf9dbb3356794a31c3
James Moore <james816am@gmail.com>
2021-10-18 19:20:51 -0400

    Basic functionality in place, search returns and displays albumthumbnail, song name, and artist name, along with an audio preview.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const musicAPI = 'https://itunes.apple.com/'
+const musicAPI = 'https://proxy-itunes-api.glitch.me/'
 const searchForm = document.querySelector('#search-form')
-console.log('test')
+// const localUrl = http://localhost:3000/itunes
 
 
 searchForm.addEventListener('submit', (event) => {
@@ -12,10 +12,45 @@ searchForm.addEventListener('submit', (event) => {
 })
 
 function searchFor(searchTerm) {
-    console.log(musicAPI + '/search?term=' + searchTerm)
+    console.log(musicAPI + '/search?term=' + searchTerm + '&media=music')
     fetch(musicAPI + '/search?term=' + searchTerm)
     .then((response) => response.json())
     .then((data) => {
         console.log(data)
+        console.log(data.results[0])
+        for(let i = 0; i<data.results.length; i++)
+        {
+            createResult(data.results[i])
+        }
 })
+}
+
+function createResult(data) {
+    let resultCard = document.createElement('div');
+    document.getElementById('results-section').appendChild(resultCard);
+    // resultCard.id = data.results
+    resultCard.classList = "resultCard";
+
+    let thumbnail = document.createElement('img');
+    resultCard.appendChild(thumbnail)
+    thumbnail.classList = 'thumbnail';
+    thumbnail.src = data.artworkUrl100;
+
+    let songName = document.createElement('p');
+    resultCard.appendChild(songName)
+    songName.classList = 'songName';
+    songName.textContent = data.trackName;
+
+    let artistName = document.createElement('p')
+    resultCard.appendChild(artistName)
+    artistName.classList = 'artistName'
+    artistName.textContent = data.artistName;
+
+    let audioContainer = document.createElement('audio')
+    resultCard.appendChild(audioContainer)
+    audioContainer.classList = 'audioContainer'
+    audioContainer.setAttribute('controls', 'controls')
+    let audioPreview = document.createElement('source')
+    audioContainer.appendChild(audioPreview)
+    audioPreview.src = data.previewUrl;
 }
