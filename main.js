091695fb492a149d98c31b0b26ccb2d5a2009183
James Moore <james816am@gmail.com>
2021-10-19 16:02:02 -0400

    Search function now awaits response, replaces spaces in search terms with + as per documentation

--- main.js
+++ main.js
@@ -1,29 +1,25 @@
 const musicAPI = 'https://proxy-itunes-api.glitch.me/'
 const searchForm = document.querySelector('#search-form')
-// const localUrl = http://localhost:3000/itunes
 
 
 searchForm.addEventListener('submit', (event) => {
     console.log(document.getElementById('search-input').value)
     const searchInput = document.getElementById('search-input').value
     event.preventDefault()
-    searchFor(searchInput)
+    searchFor(searchInput.replace(" ","+"))
     searchForm.reset();
 })
 
-function searchFor(searchTerm) {
-    fetch(musicAPI + '/search?term=' + searchTerm)
-        .then((response) => response.json())
-        .then((data) => {
-            document.getElementById('results-section').innerHTML = ''
-            console.log(data)
-            console.log(data.results[0])
-            for (let i = 0; i < data.results.length; i++) {
-                if (data.results[i].kind === 'song') {
-                    createResult(data.results[i])
-                }
+async function searchFor(searchTerm) {
+    const data = await (fetch(musicAPI + '/search?term=' + searchTerm).then((response) => response.json()))
+    {
+        document.getElementById('results-section').innerHTML = ''
+        for (let i = 0; i < data.results.length; i++) {
+            if (data.results[i].kind === 'song') {
+                createResult(data.results[i])
             }
-        })
+        }
+    }
 }
 
 function createResult(data) {
