85f676ed28921031a49e47ffb20853815d4a1661
James Moore <james816am@gmail.com>
2021-10-21 01:28:37 -0400

    Everything working, bugs squashed, still a little ugly.

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 const musicAPI = 'https://proxy-itunes-api.glitch.me/'
 const searchForm = document.querySelector('#search-form')
 
-
+// Adds listener to submit button to take input field value and run it through
+// the search, replacing spaces with + inside the search string url
 searchForm.addEventListener('submit', (event) => {
     console.log(document.getElementById('search-input').value)
     const searchInput = document.getElementById('search-input').value
     event.preventDefault()
-    searchFor(searchInput.replace(" ","+"))
+    searchFor(searchInput.replace(" ", "+"))
     searchForm.reset();
 })
 
 async function searchFor(searchTerm) {
-    const data = await (fetch(musicAPI + '/search?term=' + searchTerm).then((response) => response.json()))
-    {
-        document.getElementById('results-section').innerHTML = ''
-        for (let i = 0; i < data.results.length; i++) {
-            if (data.results[i].kind === 'song') {
-                createResult(data.results[i])
+    try {
+        const data = await (fetch(musicAPI + '/search?term=' + searchTerm).then((response) => response.json()))
+        {
+            document.getElementById('results-section').innerHTML = ''
+            for (let i = 0; i < data.results.length; i++) {
+                if (data.results[i].kind === 'song') {
+                    createResult(data.results[i])
+                }
             }
         }
+    }
+    catch(error) {
+        alert('Please try your search again.')
     }
 }
 
 function createResult(data) {
     let resultCard = document.createElement('div');
     document.getElementById('results-section').appendChild(resultCard);
-    // resultCard.id = data.results
     resultCard.classList = "resultCard";
 
     let thumbnail = document.createElement('img');
@@ -33,15 +38,54 @@ function createResult(data) {
     thumbnail.classList = 'thumbnail';
     thumbnail.src = data.artworkUrl100;
 
+    let songDiv = document.createElement('div')
+    resultCard.appendChild(songDiv)
+    let songIcon = document.createElement('i')
+    songIcon.classList = 'fas fa-music'
+    songDiv.appendChild(songIcon)
     let songName = document.createElement('p');
-    resultCard.appendChild(songName)
+    songDiv.appendChild(songName)
     songName.classList = 'songName';
-    songName.textContent = data.trackName;
+    if (data.trackName.length > 19) {
+        songName.textContent = data.trackName.substring(0, 19) + '...'
+    }
+    else { songName.textContent = data.trackName }
 
+    let artistDiv = document.createElement('div')
+    resultCard.appendChild(artistDiv)
+    let artistIcon = document.createElement('i')
+    artistIcon.classList = 'fas fa-user'
+    artistDiv.appendChild(artistIcon)
     let artistName = document.createElement('p')
-    resultCard.appendChild(artistName)
+    artistDiv.appendChild(artistName)
     artistName.classList = 'artistName'
-    artistName.textContent = data.artistName;
+    if (data.artistName.length > 19) {
+        artistName.textContent = data.artistName.substring(0, 19) + '...'
+    }
+    else { artistName.textContent = data.artistName }
+
+    let albumDiv = document.createElement('div')
+    resultCard.appendChild(albumDiv)
+    let albumIcon = document.createElement('i')
+    albumIcon.classList = 'fas fa-record-vinyl'
+    albumDiv.appendChild(albumIcon)
+    let albumName = document.createElement('p')
+    albumDiv.appendChild(albumName)
+    albumName.classList = 'albumName'
+    if (data.collectionName.length > 19) {
+        albumName.textContent = data.collectionName.substring(0, 19) + '...'
+    }
+    else { albumName.textContent = data.collectionName }
+
+    let yearDiv = document.createElement('div')
+    resultCard.appendChild(yearDiv)
+    let yearIcon = document.createElement('i')
+    yearIcon.classList = 'fas fa-calendar-alt'
+    yearDiv.appendChild(yearIcon)
+    let yearInfo = document.createElement('p')
+    yearDiv.appendChild(yearInfo)
+    yearInfo.classList = 'yearInfo'
+    yearInfo.textContent = data.releaseDate.substring(0, 4)
 
     let audioContainer = document.createElement('audio')
     resultCard.appendChild(audioContainer)
